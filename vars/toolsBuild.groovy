fc49d1a344756d49d8a1675edf2ae3a9141d14f1
Le Gall Benoît <benoit.legall@thanksys.com>
2019-01-25 13:56:10 +0100

    add declarative pipeline for tools

---
+++ vars/toolsBuild.groovy
@@ -0,0 +1,57 @@
+#!groovy
+
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Clean') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'Default'
+                    ) {
+                        sh "mvn clean -e"
+                    }
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    withMaven (
+                            maven: 'Default',
+                            jdk: 'Default'
+                    ) {
+                        sh "mvn package -e"
+                    }
+                }
+            }
+
+            stage('Docker Build') {
+                steps {
+                    withMaven (
+                            maven: 'Default',
+                            jdk: 'Default'
+                    ) {
+                        sh "mvn dockerfile:build -e"
+                    }
+                }
+            }
+            stage('Docker Push') {
+                steps {
+                    withMaven (
+                            maven: 'Default',
+                            jdk: 'Default'
+                    ) {
+                        withCredentials([
+                                usernamePassword(credentialsId: 'docker-credentials',
+                                        usernameVariable: 'USERNAME',
+                                        passwordVariable: 'PASSWORD')]) {
+                            sh "mvn dockerfile:push -e -B -Ddockerfile.username=$USERNAME -Ddockerfile.password=$PASSWORD"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
