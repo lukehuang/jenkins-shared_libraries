698709f81275c82b277e47f088f1e506092e20ef
LE GALL Benoît <legall.benoit@gmail.com>
2019-03-02 14:53:42 +0100

    add script to build jenkin's plugins

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,51 @@
+#!groovy
+
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Clean') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'Java 8'
+                    ) {
+                        sh "mvn clean -e"
+                    }
+                }
+            }
+            stage('Compile') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'Java 8'
+                    ) {
+                        sh "mvn compile -e"
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'Java 8'
+                    ) {
+                        sh "mvn test -e -Dsurefire.useFile=false"
+                    }
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    withMaven (
+                            maven: 'Default',
+                            jdk: 'Java 8'
+                    ) {
+                        sh "mvn package -DskipTests=true -e"
+                    }
+                }
+            }
+        }
+    }
+}
