7ebc84ca127543eaac7d05c47bcdea3e7db85232
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-01 10:12:05 +0200

    declare just one ansiColor("xterm") {

--- vars/moduleBuild.groovy
+++ vars/moduleBuild.groovy
@@ -2,75 +2,74 @@
 
 def call(String sonarProjectKey, String sonarToken, String sonarOrganization = 'frogdevelopment') {
     pipeline {
-        ansiColor("xterm") {
-            agent any
+        agent any
 
-            options {
-                // Only keep the 10 most recent builds
-                buildDiscarder(logRotator(numToKeepStr: '10'))
-                disableConcurrentBuilds()
+        options {
+            // Only keep the 10 most recent builds
+            buildDiscarder(logRotator(numToKeepStr: '10'))
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
+
+        stages {
+            stage('Start') {
+                steps {
+                    // send build started notifications
+                    sendNotifications 'STARTED'
+                }
             }
-
-            stages {
-                stage('Start') {
-                    steps {
-                        // send build started notifications
-                        sendNotifications 'STARTED'
+            stage('Clean') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'OpenJ9'
+                    ) {
+                        sh "mvn clean -e"
                     }
                 }
-                stage('Clean') {
-                    steps {
-                        withMaven(
-                                maven: 'Default',
-                                jdk: 'OpenJ9'
-                        ) {
-                            sh "mvn clean -e"
-                        }
+            }
+            stage('Compile') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'OpenJ9'
+                    ) {
+                        sh "mvn compile -e"
                     }
                 }
-                stage('Compile') {
-                    steps {
-                        withMaven(
-                                maven: 'Default',
-                                jdk: 'OpenJ9'
-                        ) {
-                            sh "mvn compile -e"
-                        }
+            }
+            stage('Test') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'OpenJ9'
+                    ) {
+                        sh "mvn test -e -Dsurefire.useFile=false"
                     }
                 }
-                stage('Test') {
-                    steps {
-                        withMaven(
-                                maven: 'Default',
-                                jdk: 'OpenJ9'
-                        ) {
-                            sh "mvn test -e -Dsurefire.useFile=false"
-                        }
-                    }
-                }
-                stage('Analyse') {
-                    steps {
-                        withMaven(
-                                maven: 'Default',
-                                jdk: 'OpenJ9'
-                        ) {
-                            sh "mvn sonar:sonar \
+            }
+            stage('Analyse') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'OpenJ9'
+                    ) {
+                        sh "mvn sonar:sonar \
                                   -Dsonar.projectKey=${sonarProjectKey} \
                                   -Dsonar.organization=${sonarOrganization} \
                                   -Dsonar.host.url=https://sonarcloud.io \
                                   -Dsonar.login=${sonarToken} \
                                   -e "
-                        }
                     }
                 }
-                stage('Install') {
-                    steps {
-                        withMaven(
-                                maven: 'Default',
-                                jdk: 'OpenJ9'
-                        ) {
-                            sh "mvn install -Dmaven.test.skip=true -e "
-                        }
+            }
+            stage('Install') {
+                steps {
+                    withMaven(
+                            maven: 'Default',
+                            jdk: 'OpenJ9'
+                    ) {
+                        sh "mvn install -Dmaven.test.skip=true -e "
                     }
                 }
             }
