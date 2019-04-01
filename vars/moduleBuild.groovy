cf5f4b9995b5d680acd92b203a2be41d8a32d0e4
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-01 10:52:12 +0200

    inutile

--- vars/moduleBuild.groovy
+++ vars/moduleBuild.groovy
@@ -33,23 +33,19 @@ def call(String sonarProjectKey, String sonarToken, String sonarOrganization = '
                     sh "mvn compile -e -B"
                 }
             }
-            stage('Quality') {
-                stages {
-                    stage('Test') {
-                        steps {
-                            sh "mvn test -e -B -Dsurefire.useFile=false"
-                        }
-                    }
-                    stage('Analyse') {
-                        steps {
-                            sh "mvn sonar:sonar \
+            stage('Test') {
+                steps {
+                    sh "mvn test -e -B -Dsurefire.useFile=false"
+                }
+            }
+            stage('Analyse') {
+                steps {
+                    sh "mvn sonar:sonar \
                                   -Dsonar.projectKey=${sonarProjectKey} \
                                   -Dsonar.organization=${sonarOrganization} \
                                   -Dsonar.host.url=https://sonarcloud.io \
                                   -Dsonar.login=${sonarToken} \
                                   -e -B"
-                        }
-                    }
                 }
             }
             stage('Install') {
