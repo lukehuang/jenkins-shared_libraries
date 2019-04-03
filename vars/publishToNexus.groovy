402999c9f322b64dfa0ba15387ffdff21c68e435
LE GALL Benoît <legall.benoit@gmail.com>
2019-04-03 23:45:28 +0200

    clean script

--- vars/publishToNexus.groovy
+++ vars/publishToNexus.groovy
@@ -2,9 +2,8 @@
 
 // Upload to Nexus
 def call() {
-    def nexusCredentialsId = 'nexus'
     withCredentials([[$class: 'UsernamePasswordMultiBinding',
-                      credentialsId: nexusCredentialsId,
+                      credentialsId: 'nexus',
                       usernameVariable: 'ORG_GRADLE_PROJECT_nexusUsername',
                       passwordVariable: 'ORG_GRADLE_PROJECT_nexusPassword']]) {
         sh './gradlew publish'
