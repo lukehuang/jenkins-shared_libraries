651dbf03bf68c0952e08278fbd73c5de1db381c7
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-04 17:18:29 +0200

    test get gradle version

--- vars/buildModule.groovy
+++ vars/buildModule.groovy
@@ -26,7 +26,7 @@ def call(String sonarProjectKey, String sonarToken, String sonarOrganization = '
             stage('Assemble') {
                 steps {
                     script {
-                        def version = (readFile('gradle.properties') =~ 'version=(.+)-SNAPSHOT')[0][1]
+                        def version = (readFile('gradle.properties') =~ 'version=(.+)-SNAPSHOT')/*[0][1]*/
                         echo "gradle version : ${version}"
                     }
                     sh './gradlew assemble'
