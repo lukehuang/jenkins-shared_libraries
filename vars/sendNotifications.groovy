6b9679dc9eda56f8a8e8f46a59dd6211f6245305
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-08 11:16:48 +0200

    more details for notifcation

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -6,6 +6,7 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
  * Send notifications based on build status string
  */
 def call(RunWrapper currentBuild) {
+    // build message
     def pretext = "${currentBuild.currentResult} : ${currentBuild.projectName}"
     def title = "${env.JOB_NAME}#${env.BUILD_NUMBER}"
     def title_link = "${env.BUILD_URL}"
