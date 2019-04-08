7609b1610e85ac9d3b5117f3e12fc62ad88e8551
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-08 11:57:18 +0200

    release script correction

--- vars/releaseModule.groovy
+++ vars/releaseModule.groovy
@@ -43,7 +43,7 @@ def call() {
             }
             stage('Release') {
                 steps {
-                    sh "./gradlew tag -a ${params.VERSION} -m 'release ${params.VERSION}'"
+                    sh "git tag -a ${params.VERSION} -m 'release ${params.VERSION}'"
                     sh 'git push --follow-tags origin master'
                 }
             }
