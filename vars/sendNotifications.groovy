ddae28630262561d51074ee79f019970c8f31dfa
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-08 11:22:35 +0200

    correction pretext value

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -7,7 +7,7 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
  */
 def call(RunWrapper currentBuild) {
     // build message
-    def pretext = "${currentBuild.currentResult} : ${currentBuild.fullDisplayName}"
+    def pretext = "${currentBuild.currentResult} : ${currentBuild.fullProjectName}"
     def title = "${env.JOB_NAME}#${env.BUILD_NUMBER}"
     def title_link = "${env.BUILD_URL}"
     def color = currentBuild.currentResult == 'SUCCESS' ? 'good' : 'danger'
